67aab1f2a61a3a4c1fd12c98143fac94db11b67f
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-05-21 14:42:50 +0300

    🎨

--- js/index.js
+++ js/index.js
@@ -17,4 +17,8 @@ function background() {
     }
 }
 
-document.querySelectorAll('.button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
+document.querySelectorAll('.button').forEach(
+    button => button.innerHTML = '<div><span>' + button.textContent
+        .trim().split('')
+        .join('</span><span>') + '</span></div>'
+);
