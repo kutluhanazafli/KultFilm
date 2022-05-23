f60eb9d417362031245fe6265e0a05a5c60b0a21
Kutluhan Azaflı <kutluhan321@gmail.com>
2022-05-23 22:13:30 +0300

    🐛 JavaScript Bookmark comment line

--- js/reviews.js
+++ js/reviews.js
@@ -75,55 +75,18 @@ $(document).ready(function () {  //En üste geri dön
 });
 
 $(function() {  // Bookmark button
-    //var bookmarkOn = '<i class="fa fa-bookmark"></i>'
-    //var bookmarkOff = '<i class="fa fa-bookmark-o"></i>'
-
     $('.pp-bookmark-btn')
-        //.html( $('.pp-bookmark-btn').data('state') ? bookmarkOn : bookmarkOff )
-        //.html( $('.pp-bookmark-btn').hasClass( "active" ) ? bookmarkOn : bookmarkOff )
         .click(function() {
             var btn = $(this);
-
             var context = $(this).data("context");
             var contextAction = $(this).data("context-action");
             var contextId = $(this).data("context-id");
-            // $('#log').html(context + " " + contextAction + " " + contextId )
-
-            // if( btn.data('state') ) {
-            //    btn.data('state', false);
             if (btn.hasClass("active")) {
                 btn.removeClass("active")
-                // $getJSON
-                //btn.html(bookmarkOff);
             } else {
-                // btn.data('state', true);
                 btn.addClass("active");
-                //btn.html(bookmarkOn);
             };
         });
 
-    /*
-      updateBookmarks(action, context, context-action, context-id) {
-
-      }
-      */
-    //     $('form').html('asfafaf');
-    //     var btn = $('form').attr('action');
-    //     var jqxhr = $.ajax({
-    //         url: '/echo/html/',
-    //         dataType: 'json',
-    //         data:{ id: $('form input').val() }
-    //     })
-    //     .success(function(data) {
-    //         alert("success"+data);
-    //     })
-    //     .error(function(err) {
-    //         alert("error"+err);
-    //     })
-    //     .complete(function(stuff) {
-    //         alert("complete"+stuff);
-    //     });
-    //
-    //     e.preventDefault();
 
 });
